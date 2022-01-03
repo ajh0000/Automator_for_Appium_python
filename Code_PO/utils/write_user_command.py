ed2a76ec8347fb2fb6a194c022ae1784bd9005fb
ajh0000 <869625828@qq.com>
2022-01-03 16:55:53 +0800

    UPdata

--- Code_PO/utils/write_user_command.py
+++ Code_PO/utils/write_user_command.py
@@ -1,11 +1,12 @@
-#coding = utf-8
+# coding = utf-8
 # '''
 # 这个文件是为了写入和读取config目录下的yaml文件
 # 该yaml文件，是为了储存提供Appium启动所需的信息。如：连接设备信息、生成的-p和-bp端口信息
 # '''
 
-import sys	#需要单独使用的时候，加上这个全局路径申明
+import sys  # 需要单独使用的时候，加上这个全局路径申明
 sys.path.append('D:/Github/Automator_for_Appium_python/Code_PO')
+
 import yaml
 
 # with open("../config/userconfig.yaml") as fr:
@@ -14,64 +15,66 @@ import yaml
 
 class WriteUserCommand(object):
 
-	# def __init__(self, arg):
-	# 	pass
+    # def __init__(self, arg):
+    # 	pass
 
-	def Read_data(self):
-		# '''
-		# 读取config目录下的yaml文件
-		# '''
-		with open("../config/UserConfig.yaml") as fr:
-			data = yaml.load(fr)
-			return data
+    def Read_data(self):
+        # '''
+        # 读取config目录下的yaml文件
+        # '''
+        with open("../config/UserConfig.yaml") as fr:
+            data = yaml.load(fr)
+            return data
 
-	def Get_value(self,key,port):
-		# '''
-		# 获取value的值
-		# '''
-		data = self.Read_data()
-		try:
-			value = data[key][port]
-			return value
-		except KeyError:
-			print('在yaml文件内，找不到该值')
-			return None
-		# value = data[key][port]
-		# return value
+    def Get_value(self, key, port):
+        # '''
+        # 获取value的值
+        # '''
+        data = self.Read_data()
+        try:
+            value = data[key][port]
+            return value
+        except KeyError:
+            print('在yaml文件内，找不到该值')
+            return None
 
-	def Write_data(self,i,device,port,bp):
-		# '''
-		# 写入数据到yaml文件
-		# '''
-		data = self.join_data(i,device,port,bp)
-		with open("../config/UserConfig.yaml","a") as fw:
-			yaml.dump(data,fw)
+    # value = data[key][port]
+    # return value
 
-	def join_data(self,i,device,port,bp):
-		# '''
-		# 以字典的格式，拼接数据
-		# '''
-		data = {
-		"user_info_{}".format(i):{
-		"deviceName":device,
-		"port":port,
-		"bp":bp
-		}		}
-		return data
+    def Write_data(self, i, device, port, bp):
+        # '''
+        # 写入数据到yaml文件
+        # '''
+        data = self.join_data(i, device, port, bp)
+        with open("../config/UserConfig.yaml", "a") as fw:
+            yaml.dump(data, fw)
 
-	def clear_data(self):
-		# '''
-		# 清除已有的数据
-		# '''
-		with open("../config/UserConfig.yaml","w") as fc:
-			fc.truncate()
-		fc.close()
+    def join_data(self, i, device, port, bp):
+        # '''
+        # 以字典的格式，拼接数据
+        # '''
+        data = {
+            "user_info_{}".format(i): {
+                "deviceName": device,
+                "port": port,
+                "bp": bp
+            }}
+        return data
 
-	def get_file_lines(self):
-		# 获取数据的行数
-		data = self.Read_data()
-		return len(data)
+    def clear_data(self):
+        # '''
+        # 清除已有的数据
+        # '''
+        with open("../config/UserConfig.yaml", "w") as fc:
+            fc.truncate()
+        fc.close()
+
+    def get_file_lines(self):
+        # 获取数据的行数
+        data = self.Read_data()
+        return len(data)
+
 
 if __name__ == '__main__':
-	WF = WriteUserCommand()
-	print(WF.join_data())
+    WF = WriteUserCommand()
+    print(WF.join_data())
